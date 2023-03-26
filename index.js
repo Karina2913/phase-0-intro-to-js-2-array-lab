7e2a79d98b83fdafc04dcf20901338a110972564
Karina Pliego <karinapliegovergara@gmail.com>
2023-03-26 18:37:40 -0500

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyAppendCat = [...cats, name];
+    return copyAppendCat;
+}
+
+function prependCat(name) {
+    const copyPrependCat = [name, ...cats];
+    return copyPrependCat;
+}
+
+function removeLastCat() {
+    const copyRemoveLastCat = cats.slice(0, cats.length - 1);
+    return copyRemoveLastCat;
+}
+
+function removeFirstCat() {
+    const copyRemoveFirstCat = cats.slice(1);
+    return copyRemoveFirstCat;
+}
